eda9cd051177dce74bdb4c6aef0bbae51bc953c0
alexbcberio <alexbcberio@gmail.com>
2021-10-27 18:06:24 +0200

    ✨ Add cmd field with Command (from commander)

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,7 +1,10 @@
+import { Command as Commander } from "commander";
+
 interface Command {
   name: string;
   get usage(): string;
   execute(args: Array<string>): Promise<void>;
+  cmd?: Commander;
 }
 
 export { Command };
