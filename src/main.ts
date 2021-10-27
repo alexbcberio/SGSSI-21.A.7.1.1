3ff91ec6124b01c235dd7a1c28b91865559a7164
alexbcberio <alexbcberio@gmail.com>
2021-10-27 18:07:34 +0200

    🚧 Adapt code to work with both implementations

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { getHashes } from "crypto";
 
 function showHelp() {
   // eslint-disable-next-line no-magic-numbers
-  const usage = `${process.argv[0]} ${process.argv[1]} <command> <args>`;
+  const usage = `\nUsage: <command> ...`;
   const commandsNameUsage = commands.map((c) => `${c.name} ${c.usage}`);
-  const availableHashes = getHashes().join(", ");
+  const availableHashes = getHashes()
+    .map((v) => `  ${v}`)
+    .join("\n");
 
   console.log(usage);
   console.log(
@@ -14,14 +16,12 @@ function showHelp() {
       .map((n) => `  ${n}`)
       .join("\n")}`
   );
-  console.log(`\n\nSupported hashes:\n${availableHashes}`);
+  console.log(`\nSupported hash algorithms:\n${availableHashes}`);
 }
 
 (async function () {
   // eslint-disable-next-line no-magic-numbers
-  const argv = process.argv.splice(2);
-
-  const commandName = argv.shift();
+  const commandName = process.argv.splice(2, 1).shift();
   const command = commands.find((c) => c.name === commandName);
 
   if (!command) {
@@ -29,5 +29,15 @@ function showHelp() {
     process.exit(errorExitCode);
   }
 
-  await command.execute(argv);
+  if (!command.cmd) {
+    // eslint-disable-next-line no-magic-numbers
+    await command.execute(process.argv.splice(2));
+
+    process.exit();
+  }
+
+  const { cmd } = command;
+
+  cmd.showHelpAfterError();
+  await cmd.parseAsync(process.argv);
 })();
